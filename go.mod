b92574702edf68db79727762da4a796bdf1e9db2
Joao Ferreira <joao.ferreira@nomadtech.pt>
2021-07-27 00:04:33 +0100

    - Add serial to tcp code - Add to code examples from ch8, clock and netcat - Update go modules list

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jmiguelff/go-boiler
+
+go 1.16
+
+require (
+	github.com/stianeikeland/go-rpio v4.2.0+incompatible
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ ser2net/ser2net.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+
+	"github.com/tarm/serial"
+)
+
+func main() {
+	dstPtr := flag.String("dst", "localhost:8000", "data destinantion")
+	flag.Parse()
+
+	c := &serial.Config{Name: "/dev/ttymxc4", Baud: 9600}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listener, err := net.Listen("tcp", *dstPtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	log.Print("listening on " + *dstPtr)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		log.Print("Got a connection")
+		go serialToTcp(conn, s)
+		go tcpToSerial(conn, s)
+	}
+
+}
+
+func serialToTcp(c net.Conn, s *serial.Port) {
+	buf := make([]byte, 64)
+	for {
+		n, err := s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.Write(buf[:n])
+	}
+}
+
+func tcpToSerial(c net.Conn, s *serial.Port) {
+	buf := make([]byte, 64)
+	for {
+		n, err := c.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s.Write(buf[:n])
+	}
+}
