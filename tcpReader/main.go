68ff7058846f46fecf0797b51255980a8f1b707b
Joao Ferreira <jmiguelff@gmail.com>
2023-05-01 22:26:04 +0100

    Add TCP client to receive data

---
+++ tcpReader/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+)
+
+func main() {
+	dstPtr := flag.String("dst", "localhost:8000", "data destinantion")
+	flag.Parse()
+
+	// Get the IP struct from hostname
+	tcpAddr, err := net.ResolveTCPAddr("tcp", *dstPtr)
+	if err != nil {
+		log.Fatalln("Resolve address fail ", err)
+	}
+
+	// Connect to the server
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		log.Fatalln("TCP connection fail ", err)
+	}
+	// Close socket after return from main
+	defer conn.Close()
+
+	for {
+		buffer := make([]byte, 1400)
+		dataSize, err := conn.Read(buffer)
+		if err != nil {
+			log.Println("Connection closed")
+			return
+		}
+
+		// Slice just the part of the slice with data
+		data := buffer[:dataSize]
+		log.Println("Number of bytes received: ", dataSize)
+		//log.Println("Received message: ", string(data))
+		log.Printf("Received bytes:\n [%x]\n", data)
+	}
+}
