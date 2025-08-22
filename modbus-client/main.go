7aa9b6e3db0a23514da034dc996e62f06727c225
Joao Ferreira <jmiguelff@gmail.com>
2025-08-22 09:28:23 +0100

    small change on the cli just to be able to select ip and port

--- modbus-client/main.go
+++ modbus-client/main.go
@@ -10,20 +10,32 @@ import (
 )
 
 func main() {
-	handler := modbus.NewTCPClientHandler("192.168.1.68:502")
+	if len(os.Args) < 3 {
+		fmt.Println("Uso: go run main.go <IP> <Porta>")
+		os.Exit(1)
+	}
+
+	ip := os.Args[1]
+	port := os.Args[2]
+	address := fmt.Sprintf("%s:%s", ip, port)
+
+	handler := modbus.NewTCPClientHandler(address)
 	handler.Timeout = 10 * time.Second
 	handler.SlaveID = 0x01
-	handler.Logger = log.New(os.Stdout, "test: ", log.LstdFlags)
-	// Connect manually so that multiple requests are handled in one connection session
+	handler.Logger = log.New(os.Stdout, "modbus: ", log.LstdFlags)
+
+	// Connect
 	err := handler.Connect()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("Erro ao ligar:", err)
 	}
 	defer handler.Close()
 
 	client := modbus.NewClient(handler)
-	//res, err := client.ReadDiscreteInputs(1, 2)
-	res, err := client.ReadInputRegisters(3001, 6)
-	// res, err := client.ReadHoldingRegisters(3001, 6)
-	fmt.Println(res)
+
+	res, err := client.ReadHoldingRegisters(0, 10) // lê 40001 a 40010
+	if err != nil {
+		log.Fatalln("Erro na leitura:", err)
+	}
+	fmt.Printf("Resposta: %v\n", res)
 }
